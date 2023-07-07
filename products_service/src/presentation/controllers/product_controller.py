863b9156392e3a894baa621597735a847e6256bd
eduardoyanoliveira <63554709+eduardoyanoliveira@users.noreply.github.com>
2023-07-06 22:39:15 -0300

    Finished in progress product_category commands

--- products_service/src/presentation/controllers/product_controller.py
+++ products_service/src/presentation/controllers/product_controller.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from contracts.ProductCategoryContracts.CreateProductCategory import CreateProductCategoryRequest
+from contracts.ProductContracts.CreateProductRequest import CreateProductRequest
+from typing import int
 
 from domain_services.DependencyInjectionContainer import provide_list_products_command
-from infra.DependencyInjectionContainer import get_db
+from infra.DependencyInjectionContainer import get_db, provide_product_category_repository, provide_product_repository
 
 product_router = APIRouter()
 
@@ -13,11 +16,21 @@ def index():
     return products
 
 @product_router.get("/products/{product_id}")
-def get_product(product_id: int, db: Session = Depends(get_db)):
-    product = product_repository(db).get_product_by_id(product_id)
-    return {"product": product}
+def get_product(pk: int, db: Session = Depends(get_db)):
+    result = provide_product_repository(db).get_product_by_id(pk)
+    return {"product": result}
 
 @product_router.post("/products")
-def create_product(product: Product, db: Session = Depends(get_db)):
-    created_product = product_repository(db).create_product(product)
-    return {"product": created_product}
+def create_product(request: CreateProductRequest, db: Session = Depends(get_db)):
+    result = provide_product_repository(db).create_product(request)
+    return {"product": result}
+
+@product_router.post("/product_categories")
+def create_product(request: CreateProductCategoryRequest, db: Session = Depends(get_db)):
+    result = provide_product_category_repository(db).create(request)
+    return {"product_category": result}
+
+@product_router.posgett("/product_categories/{product_category_id}")
+def create_product(pk: int, db: Session = Depends(get_db)):
+    result = provide_product_category_repository(db).create(pk)
+    return {"product_category": result}
